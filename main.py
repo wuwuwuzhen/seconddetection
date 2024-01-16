7ab154be846bfea2a7cd9ed95c70c836a8053d06
jglicat <jglicat@outlook.com>
2024-01-17 01:04:09 +0800

    删除垃圾

--- main.py
+++ main.py
@@ -14,37 +14,18 @@ CORS(app)  # 允许跨域请求
 photo_path = './picture/'
 video_path = './video/'
 url = 'http://10.2.137.136:9202/alarm/filter/receive'
-logging.basicConfig(filename='log.log', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
+logging.basicConfig(filename='log.log', level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
 
-
-# @app.route('/seconddetection/', methods=['POST'])
-# def seconddetection():
-#     df = pd.DataFrame(request.json)
-#     # thread_pool_executor(df)
-#     df_combined = sample_selection(df)
-#     json_payload = pack_req(df_combined)   
-#     response = requests.post(url, json=json_payload)
-#     if response.status_code == 200:
-#         print('Request sent successfully.')
-#     else:
-#         print(f'Failed to send request: {response.status_code}, {response.text}')
-    
-#     df_dict = df_combined.to_dict(orient='records')
-
-#     # 需要保留中文不转义
-#     res = json.dumps(df_dict, ensure_ascii=False)
-#     return res, 200
 
 @app.route('/seconddetection/', methods=['POST'])
 def seconddetection():
     logging.info(f'Request received: {request.json}')
     try:
         df = pd.DataFrame(request.json)
-        # df.to_csv('test.csv')
-        # df.to_excel('test.xlsx')
-        image_thread_pool_executor(df) 
+        image_thread_pool_executor(df)
         df_combined = sample_selection(df)
-        json_payload = pack_req(df_combined)   
+        json_payload = pack_req(df_combined)
         logging.info(f'{json_payload}')
         response = requests.post(url, json=json_payload)
         if response.status_code == 200:
@@ -52,12 +33,14 @@ def seconddetection():
             return jsonify({"message": "Request sent successfully", "data": response.json()}), 200
         else:
             # 如果请求失败，返回状态码 500 和错误信息
-            logging.error(f'Failed to send request: {response.status_code}, {response.text}')
+            logging.error(
+                f'Failed to send request: {response.status_code}, {response.text}')
             return jsonify({"message": "Failed to send request", "error": response.text}), 500
     except Exception as e:
         # 如果在处理中出现异常，返回状态码 500 和异常信息
         logging.error(f'An error occurred: {str(e)}')
         return jsonify({"message": "An error occurred", "error": str(e)}), 500
+
 
 with app.app_context():
     if not os.path.exists(photo_path):
