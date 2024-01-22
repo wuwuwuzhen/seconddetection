71220f77517047715b09dc428e749638298337a8
Wuzhen0823 <357337661@qq.com>
2024-01-22 13:53:06 +0800

    fix bugs

--- fv_detection.py
+++ fv_detection.py
@@ -124,7 +124,7 @@ def vehicle_collision(Test_path):
                                 Flag[i] = 1
                 if Flag[i] != 1:
                     Flag[i] =2
-        dir_path = os.path.join(yolo_model_path.root_path, 'yolo_picture')
+        dir_path = os.path.join(model_path.root_path, 'yolo_picture')
         for filename in os.listdir(dir_path):
             # 构造文件的完整路径
             file_path = os.path.join(dir_path, filename)
